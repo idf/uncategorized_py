6db53a25729e703d7b5c475e0e31ac561dd4e14b
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-11-21 19:03:02 -0800

    circuits

---
+++ circuits.py
@@ -0,0 +1,21 @@
+"""
+Full Subtractor
+http://www.flintgroups.com/2012/10/half-subtractor-and-full-subtractor.html
+"""
+def half_sub(a, b):
+    borrow = ~a & b
+    out   = a ^ b
+    return out, borrow
+
+def full_sub(a, b, bin):
+    out, b1 = half_sub(a, b)
+    out, b2 = half_sub(out, bin)
+    bout = b1 | b2
+    return out, bout
+
+
+if __name__ == "__main__":
+    for a in (0, 1):
+        for b in (0, 1):
+            for bin in (0, 1):
+                print a, b, bin, full_sub(a, b, bin)
